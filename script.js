b3eb11ccf460abb12817ab3a27d24603768f53c7
onetayjones <taylordjone@gmail.com>
2022-08-29 17:29:15 +0400

    Beginner Lost Without a Map

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function greet(name){
 }
 
 console.log(greet("Taylor"))
+
+
+// Beginner - Lost Without a Map
+
+// Given an array of integers, return a new array with each value doubled.
+// For example: [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+ let NewArr = [];
+  for(let i =0; i < x.length; i++){
+    NewArr.push(x[i] * 2)
+  }
+  return NewArr;
+}
+
+console.log(maps([2,4,5]))
