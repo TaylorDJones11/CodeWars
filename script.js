ea4a7fb4abf6451ab2639292cfd40e8b7e41db19
onetayjones <taylordjone@gmail.com>
2022-09-01 19:35:02 +0400

    Code Wars Problem/solution

--- script.js
+++ script.js
@@ -45,3 +45,21 @@ function filter_list(l) {
     }
   })
 }
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+const arr = numbers.split(" ")
+    let maxNum = arr[0]
+    let minNum = arr[0]
+
+    for(let num of arr) {
+        if(Number(num) > maxNum) {
+            maxNum = num
+        }
+
+        if(Number(num) < minNum) {
+            minNum = num
+        }
+    }
+
+    return maxNum + " " + minNum
